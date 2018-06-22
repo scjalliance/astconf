ed93d3e67b3a8f3f3991003379dd8078b3a7418a
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-06-22 15:59:18 -0700

    dpma: Added tags to phone fields

--- dpma/phone.go
+++ dpma/phone.go
@@ -1,74 +1,77 @@
 package dpma
 
+import (
+	"strings"
+
+	"github.com/scjalliance/astconf"
+)
+
+// https://wiki.asterisk.org/wiki/display/DIGIUM/DPMA+Configuration#DPMAConfiguration-PhoneConfigurationOptions
+
 // Phone is a DPMA phone definition.
 type Phone struct {
-	Network                  string
-	Firmware                 string
-	MAC                      string
-	PIN                      int
-	GroupPIN                 int
-	Line                     string
-	ExternalLine             string
-	Application              string
-	ConfigFile               string
-	FullName                 string
-	Contact                  string
-	ContactsDisplayRules     string
-	BLFContactGroup          string
-	BLFItems                 string
-	BLFPageReturnTimeout     string
-	ContactsMaxSubscriptions int
-	Timezone                 string
-	NTPResync                int
-	ParkingExtension         string
-	ParkingTransferType      string // "blind", "attended"
-	ShowCallParking          bool
-	Ringtones                []string
-	WebUIEnabled             bool
-	RecordOwnCalls           bool
-	CanForwardCalls          bool
-	ShowCallLog              bool
-	Alerts                   []string
-	MulticastPage            []string
-	BLFUnusedLineKeys        bool
-	SendToVoicemail          bool
-	D40LogoFile              string
-	D45LogoFile              string
-	D50LogoFile              string
-	D60LogoFile              string
-	D62LogoFile              string
-	D65LogoFile              string
-	D70LogoFile              string
-	D80LogoFile              string
-	WallpaperFile            string
-	EHS                      string // "auto", "plantronics", "jabra_iq"
-	LockPreferences          bool
-	LoginPassword            int
-	AcceptLocalCalls         string // "any", "host"
-	DisplayMCNotification    string
-	IdleCompanyText          string
-	SmallClock               bool
-	DefaultFontSize          int
-	Brightness               int
-	Contrast                 int
-	DimBacklight             bool
-	BacklightTimeout         int
-	BacklightDimLevel        int
-	ActiveLocale             string
-	RingerVolume             int
-	SpeakerVolume            int
-	HandsetVolume            int
-	HeadsetVolume            int
-	CallWaitingTone          bool
-	HandsetSidetoneDB        int
-	HeadsetSidetoneDB        int
-	ResetCallVolume          bool
-	HeadsetAnswer            bool
-	RingHeadsetOnly          bool
-	NameFormat               string // "first_last", "last_first"
-	LanPortMode              string // "auto", "10hd", "10fd", "100hd", "100fd", "1000fd"
-	PCPortMode               string // "auto", "10hd", "10fd", "100hd", "100fd", "1000fd"
-	EnableCheckSync          bool
+	Networks                 []string `astconf:"network"`
+	Firmware                 []string `astconf:"firmware"`
+	MAC                      string   `astconf:"mac,omitempty"`
+	PIN                      int      `astconf:"pin,omitempty"`
+	GroupPIN                 int      `astconf:"group_pin,omitempty"`
+	Lines                    []string `astconf:"line"` // First entry is primary line
+	ExternalLine             string   `astconf:"external_line,omitempty"`
+	Applications             []string `astconf:"application"`
+	ConfigFile               string   `astconf:"config_file,omitempty"`
+	FullName                 string   `astconf:"full_name,omitempty"`
+	Contacts                 []string `astconf:"contact"`
+	ContactsDisplayRules     string   `astconf:"contacts_display_rules,omitempty"`
+	BLFContactGroup          string   `astconf:"blf_contact_group,omitempty"`
+	BLFItems                 string   `astconf:"blf_items,omitempty"`
+	BLFPageReturnTimeout     int      `astconf:"blf_page_return_timeout,omitempty"`
+	ContactsMaxSubscriptions int      `astconf:"contacts_max_subscriptions,omitempty"`
+	Timezone                 string   `astconf:"timezone,omitempty"`
+	NTPResync                int      `astconf:"ntp_resync,omitempty"`
+	ParkingExtension         int      `astconf:"parking_exten,omitempty"`
+	ParkingTransferType      string   `astconf:"parking_transfer_type,omitempty"` // "blind", "attended"
+	ShowCallParking          bool     `astconf:"show_call_parking,omitempty"`
+	Ringtones                []string `astconf:"ringtone,omitempty"`
+	ActiveRingtone           string   `astconf:"active_ringtone,omitempty"`
+	WebUIEnabled             bool     `astconf:"web_ui_enabled,omitempty"`
+	RecordOwnCalls           bool     `astconf:"record_own_calls,omitempty"`
+	CanForwardCalls          bool     `astconf:"can_forward_calls,omitempty"`
+	ShowCallLog              bool     `astconf:"show_call_log,omitempty"`
+	LogOutEnabled            bool     `astconf:"logout_enabled,omitempty"`
+	Alerts                   []string `astconf:"alert"`
+	MulticastPage            []string `astconf:"multicastpage"`
+	BLFUnusedLineKeys        bool     `astconf:"blf_unused_linekeys,omitempty"`
+	SendToVoicemail          bool     `astconf:"send_to_vm,omitempty"`
+	LogoFiles                []LogoFile
+	WallpaperFile            string `astconf:"wallpaper_file,omitempty"`
+	EHS                      string `astconf:"ehs,omitempty"` // "auto", "plantronics", "jabra_iq"
+	LockPreferences          bool   `astconf:"lock_preferences,omitempty"`
+	LoginPassword            int    `astconf:"login_password,omitempty"`
+	AcceptLocalCalls         string `astconf:"accept_local_calls,omitempty"` // "any", "host"
+	DisplayMCNotification    bool   `astconf:"display_mc_notification,omitempty"`
+	IdleCompanyText          string `astconf:"idle_company_text,omitempty"`
+	SmallClock               bool   `astconf:"small_clock,omitempty"`
+	DefaultFontSize          int    `astconf:"default_fontsize,omitempty"`
+	Brightness               int    `astconf:"brightness,omitempty"`
+	Contrast                 int    `astconf:"contrast,omitempty"`
+	DimBacklight             bool   `astconf:"dim_backlight,omitempty"`
+	BacklightTimeout         int    `astconf:"backlight_timeout,omitempty"`
+	BacklightDimLevel        int    `astconf:"backlight_dim_level,omitempty"`
+	ActiveLocale             string `astconf:"active_locale,omitempty"`
+	RingerVolume             int    `astconf:"ringer_volume,omitempty"`
+	SpeakerVolume            int    `astconf:"speaker_volume,omitempty"`
+	HandsetVolume            int    `astconf:"handset_volume,omitempty"`
+	HeadsetVolume            int    `astconf:"headset_volume,omitempty"`
+	CallWaitingTone          bool   `astconf:"call_waiting_tone,omitempty"`
+	HandsetSidetoneDB        int    `astconf:"handset_sidetone_db,omitempty"`
+	HeadsetSidetoneDB        int    `astconf:"headset_sidetone_db,omitempty"`
+	ResetCallVolume          bool   `astconf:"reset_call_volume,omitempty"`
+	HeadsetAnswer            bool   `astconf:"headset_answer,omitempty"`
+	RingHeadsetOnly          bool   `astconf:"ring_headset_only,omitempty"`
+	NameFormat               string `astconf:"name_format,omitempty"`   // "first_last", "last_first"
+	LanPortMode              string `astconf:"lan_port_mode,omitempty"` // "auto", "10hd", "10fd", "100hd", "100fd", "1000fd"
+	PCPortMode               string `astconf:"pc_port_mode,omitempty"`  // "auto", "10hd", "10fd", "100hd", "100fd", "1000fd"
+	EnableCheckSync          bool   `astconf:"enable_check_sync,omitempty"`
 
 	// TODO: Add 802.1x parameters
 
@@ -76,5 +79,16 @@ type Phone struct {
 
 	// TODO: Add OpenVPN parameters
 
-	TransportTLSAllowed bool
+	TransportTLSAllowed bool `astconf:"transport_tls_allowed"`
+}
+
+// SectionName returns the name of the ringtone section.
+func (p *Phone) SectionName() string {
+	// FIXME: Generate an alternate name if the phone doesn't have a MAC
+	return strings.ToLower(strings.Replace(string(p.MAC), ":", "", -1))
+}
+
+// MarshalAsteriskPreamble marshals the type.
+func (p *Phone) MarshalAsteriskPreamble(e *astconf.Encoder) error {
+	return e.Printer().Setting("type", "phone")
 }

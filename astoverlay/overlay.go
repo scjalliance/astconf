4d204aaabccfffcd650d019078976f6a7cee1e20
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-03-19 12:33:09 -0700

    astoverlay: Documentation fixes

--- astoverlay/overlay.go
+++ astoverlay/overlay.go
@@ -7,63 +7,81 @@ import (
 	"github.com/scjalliance/astconf/astval"
 )
 
-// SectionName merges two asterisk section names.
+// SectionName overlays two asterisk section names.
+//
+// If from is non-empty, the value of to will be replaced with from.
 func SectionName(from, to *astconf.SectionName) {
 	if *from != "" {
 		*to = *from
 	}
 }
 
-// AstYesNoNone merges two asterisk YesNoNone values.
+// AstYesNoNone overlays two asterisk YesNoNone values.
+//
+// If from holds a specified value, the value of to will be replaced with from.
 func AstYesNoNone(from, to *astval.YesNoNone) {
 	if (*from).Specified() {
 		*to = *from
 	}
 }
 
-// Int merges two int values.
+// Int overlays two int values.
+//
+// If from is non-empty, the value of to will be replaced with from.
 func Int(from, to *int) {
 	if *from != 0 {
 		*to = *from
 	}
 }
 
-// AstInt merges two asterisk Int values.
+// AstInt overlays two asterisk Int values.
+//
+// If from holds a specified value, the value of to will be replaced with from.
 func AstInt(from, to *astval.Int) {
 	if (*from).Specified() {
 		*to = *from
 	}
 }
 
-// String merges two string values.
+// String overlays two string values.
+//
+// If from is non-empty, the value of to will be replaced with from.
 func String(from, to *string) {
 	if *from != "" {
 		*to = *from
 	}
 }
 
-// AstString merges two asterisk String values.
+// AstString overlays two asterisk String values.
+//
+// If from holds a specified value, the value of to will be replaced with from.
 func AstString(from, to *astval.String) {
 	if (*from).Specified() {
 		*to = *from
 	}
 }
 
-// StringSlice merges two string slices.
+// StringSlice overlays two string slices.
+//
+// If from is non-empty, the value of to will be replaced with from.
 func StringSlice(from, to *[]string) {
 	if *from != nil {
 		*to = *from
 	}
 }
 
-// Duration merges two duration values.
+// Duration overlays two duration values.
+//
+// If from is non-empty, the value of to will be replaced with from.
 func Duration(from, to *time.Duration) {
 	if *from != 0 {
 		*to = *from
 	}
 }
 
-// AstSeconds merges two asterisk Seconds values.
+// AstSeconds overlays two asterisk Seconds values.
+//
+// If from holds a specified value, the value of to will be replaced with from.
 func AstSeconds(from, to *astval.Seconds) {
 	if (*from).Specified() {
 		*to = *from

89296633c9f3b3369b42bc9d45c4bf6e315dba82
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-04-02 16:09:43 -0700

    dpma: Improved Alert and Ringtone definitions

--- dpma/alert.go
+++ dpma/alert.go
@@ -1,8 +1,21 @@
 package dpma
 
+import "github.com/scjalliance/astconf"
+
 // Alert is a DPMA alert definition.
 type Alert struct {
-	Info     string `astconf:"alert_info"`
-	RingType string `astconf:"ring_type"`
-	RingTone string `astconf:"ring_tone"`
+	Name       string `astconf:"-"`
+	InfoHeader string `astconf:"alert_info"`
+	RingType   string `astconf:"ring_type"`
+	Ringtone   string `astconf:"ringtone"`
+}
+
+// SectionName returns the name of the ringtone section.
+func (a *Alert) SectionName() string {
+	return a.Name
+}
+
+// MarshalAsteriskPreamble marshals the type.
+func (*Alert) MarshalAsteriskPreamble(e *astconf.Encoder) error {
+	return e.Printer().Setting("type", "ringtone")
 }
--- dpma/ringtone.go
+++ dpma/ringtone.go
@@ -1,23 +1,22 @@
 package dpma
 
 import (
-	"strings"
-
 	"github.com/scjalliance/astconf"
 )
 
 // Ringtone is a DPMA ringtone definition.
 type Ringtone struct {
+	Name     string `astconf:"-"`
 	Alias    string `astconf:"alias"`
 	Filename string `astconf:"filename"`
 }
 
 // SectionName returns the name of the ringtone section.
 func (r *Ringtone) SectionName() string {
-	return strings.ToLower(strings.Replace(string(r.Alias), " ", "-", -1))
+	return r.Name
 }
 
 // MarshalAsteriskPreamble marshals the type.
-func (r *Ringtone) MarshalAsteriskPreamble(e *astconf.Encoder) error {
+func (*Ringtone) MarshalAsteriskPreamble(e *astconf.Encoder) error {
 	return e.Printer().Setting("type", "ringtone")
 }

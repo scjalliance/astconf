8fa2b2bcaaf218c196ff35de6636d53788d423d3
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-04-05 15:16:07 -0700

    astgen: Moved paging group and phone extensions to separate files

--- astgen/paginggroupextension.go
+++ astgen/paginggroupextension.go
@@ -8,60 +8,6 @@ import (
 	"github.com/scjalliance/astconf/dialplan"
 )
 
-// PhoneExtensions generates a dialplan section that includes all phone
-// extensions in a dataset.
-func PhoneExtensions(data *astorg.DataSet, context string) dialplan.Section {
-	section := dialplan.Section{Context: context}
-
-	for _, person := range data.People {
-		if person.Extension == "" || person.Username == "" {
-			continue
-		}
-
-		device := dialplan.SIP(person.Username)
-		presence := fmt.Sprintf("CustomPresence:%s", person.Username)
-
-		ext := dialplan.Extension{
-			Comment: person.FullName,
-			Number:  person.Extension,
-			Actions: []dialplan.Action{
-				dialplan.Noop(fmt.Sprintf("Call %s", person.Username)),
-				dialplan.Macro("pre-answer", dialplan.Var("EXTEN")),
-				dialplan.ExecIf(dialplan.Equal(dialplan.DeviceState(device), dialplan.String("NOT_INUSE")), dialplan.Dial(device, 20)),
-				dialplan.ExecIf(dialplan.Equal(dialplan.PresenceState(presence, "subtype"), dialplan.String("with Call Waiting Enabled")), dialplan.Dial(device, 20)),
-				dialplan.Macro("no-answer", dialplan.Var("EXTEN")),
-			},
-		}
-
-		section.Extensions = append(section.Extensions, ext)
-	}
-
-	for _, role := range data.PhoneRoles {
-		if role.Extension == "" || role.Username == "" {
-			continue
-		}
-
-		device := dialplan.SIP(role.Username)
-		presence := fmt.Sprintf("CustomPresence:%s", role.Username)
-
-		ext := dialplan.Extension{
-			Comment: role.DisplayName,
-			Number:  role.Extension,
-			Actions: []dialplan.Action{
-				dialplan.Noop(fmt.Sprintf("Call %s", role.Username)),
-				dialplan.Macro("pre-answer", dialplan.Var("EXTEN")),
-				dialplan.ExecIf(dialplan.Equal(dialplan.DeviceState(device), dialplan.String("NOT_INUSE")), dialplan.Dial(device, 20)),
-				dialplan.ExecIf(dialplan.Equal(dialplan.PresenceState(presence, "subtype"), dialplan.String("with Call Waiting Enabled")), dialplan.Dial(device, 20)),
-				dialplan.Congestion(),
-			},
-		}
-
-		section.Extensions = append(section.Extensions, ext)
-	}
-
-	return section
-}
-
 // PagingGroupExtensions generates a dialplan section that includes all
 // paging group extensions in a dataset.
 func PagingGroupExtensions(data *astorg.DataSet, context string) dialplan.Section {

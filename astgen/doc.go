0ab7769e478339863d22c809c1681906447556df
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-04-05 16:19:13 -0700

    astgen: Added documentation note about planned changes

--- astgen/doc.go
+++ astgen/doc.go
@@ -1,3 +1,9 @@
 // Package astgen is a highly experimental package that converts astorg
 // datasets into DPMA-based phone configuration files.
+//
+// TODO: Refactor the functions in this package to avoid ingestion of whole
+// datasets. Taking a dataset as an argument hides the actual requirements
+// of each generator. Fixing this will probably require that we redesign
+// the generator functions to cumulatively apply the astorg phone, person
+// and role objects in separate functions.
 package astgen
